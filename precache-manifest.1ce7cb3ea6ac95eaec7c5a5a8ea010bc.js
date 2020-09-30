2c5a7a3643065e420ae7b7a538308c9fda368db4
Efim1298 <efimchuk12@gmail.com>
2020-09-30 19:44:05 +0300

    Updates

---
+++ precache-manifest.1ce7cb3ea6ac95eaec7c5a5a8ea010bc.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "943013afb7dc8d7c87a24bdac8e3b4be",
+    "url": "/React_website/index.html"
+  },
+  {
+    "revision": "fa266f19eb1ea90fd17b",
+    "url": "/React_website/static/css/main.b0dc5267.chunk.css"
+  },
+  {
+    "revision": "a5db3d41905da993525a",
+    "url": "/React_website/static/js/2.723d6b85.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_website/static/js/2.723d6b85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa266f19eb1ea90fd17b",
+    "url": "/React_website/static/js/main.7cbe133a.chunk.js"
+  },
+  {
+    "revision": "54ee2fcf83424e6087e4",
+    "url": "/React_website/static/js/runtime-main.6c352c3d.js"
+  },
+  {
+    "revision": "d6ab0b86ccf901230835d8bd714cd8d1",
+    "url": "/React_website/static/media/home1.d6ab0b86.svg"
+  },
+  {
+    "revision": "79ba2002d56019901132986d5c84067a",
+    "url": "/React_website/static/media/home2(profile).79ba2002.jpg"
+  },
+  {
+    "revision": "dd2288fc6be04bcbf8fda8efc830ea0f",
+    "url": "/React_website/static/media/home3.dd2288fc.svg"
+  },
+  {
+    "revision": "397c455a49d4f872c292a3dc25432579",
+    "url": "/React_website/static/media/home4.397c455a.svg"
+  },
+  {
+    "revision": "f4077dc6d4e5ac4498b9f698fdbe4133",
+    "url": "/React_website/static/media/img-2.f4077dc6.svg"
+  },
+  {
+    "revision": "1ce343fc4f263c681d19635cebc7ebc2",
+    "url": "/React_website/static/media/img-3.1ce343fc.svg"
+  },
+  {
+    "revision": "6ad60d0ba6a0f726c82d9f14c336ab60",
+    "url": "/React_website/static/media/prod1.6ad60d0b.svg"
+  },
+  {
+    "revision": "d73db671aef3e57591e0e1959602d800",
+    "url": "/React_website/static/media/prod2.d73db671.svg"
+  },
+  {
+    "revision": "be3a343e473c404057bdb19e6665087e",
+    "url": "/React_website/static/media/serv1.be3a343e.svg"
+  },
+  {
+    "revision": "885dc68bf623fcf56cd981b084d93006",
+    "url": "/React_website/static/media/serv2.885dc68b.svg"
+  }
+]);
