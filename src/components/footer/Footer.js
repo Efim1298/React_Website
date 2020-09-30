fdab8d5672323f39e50afda673e6514080b2efc8
Efim1298 <efimchuk12@gmail.com>
2020-09-30 19:35:39 +0300

    deploy my react project

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,138 @@
+import React from "react";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin
+} from 'react-icons/fa';
+import { Button } from "../../globalStyles";
+import {
+  FooterContainer,
+  FooterSubscription,
+  FooterSubText,
+  FooterSubHeading,
+  Form,
+  FormInput,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  SocialIcon,
+  WebsiteRights,
+  SocialIcons,
+  SocialIconLink
+} from "./Footer.elements";
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterSubscription>
+        <FooterSubHeading>
+          Join our exclusive membership to receive the latest news and trends
+        </FooterSubHeading>
+        <FooterSubText>You can unsubscribe at any time.</FooterSubText>
+        <Form>
+          <FormInput name="email" type="email" placeholder="Your Email" />
+          <Button fontBig>Subscribe</Button>
+        </Form>
+      </FooterSubscription>
+      <FooterLinksContainer>
+        <FooterLinksWrapper>
+          <FooterLinkItems>
+            <FooterLinkTitle>About Us</FooterLinkTitle>
+            <FooterLink to="/sign-up">How it works</FooterLink>
+            <FooterLink to="/">Testimonials</FooterLink>
+            <FooterLink to="/">Careers</FooterLink>
+            <FooterLink to="/">Investors</FooterLink>
+            <FooterLink to="/">Terms of Service</FooterLink>
+          </FooterLinkItems>
+          <FooterLinkItems>
+            <FooterLinkTitle>Contact Us</FooterLinkTitle>
+            <FooterLink to="/">Contact</FooterLink>
+            <FooterLink to="/">Support</FooterLink>
+            <FooterLink to="/">Destinations</FooterLink>
+            <FooterLink to="/">Sponsorships</FooterLink>
+          </FooterLinkItems>
+        </FooterLinksWrapper>
+        <FooterLinksWrapper>
+          <FooterLinkItems>
+            <FooterLinkTitle>Videos</FooterLinkTitle>
+            <FooterLink to="/">Submit Video</FooterLink>
+            <FooterLink to="/">Ambassadors</FooterLink>
+            <FooterLink to="/">Agency</FooterLink>
+            <FooterLink to="/">Influencer</FooterLink>
+          </FooterLinkItems>
+          <FooterLinkItems>
+            <FooterLinkTitle>Social Media</FooterLinkTitle>
+            <FooterLink to="/">Instagram</FooterLink>
+            <FooterLink to="/">Facebook</FooterLink>
+            <FooterLink to="/">Youtube</FooterLink>
+            <FooterLink to="/">Twitter</FooterLink>
+          </FooterLinkItems>
+        </FooterLinksWrapper>
+      </FooterLinksContainer>
+      <SocialMedia>
+        <SocialMediaWrap>
+          <SocialLogo to="/">
+            <SocialIcon />
+            eFIMa
+          </SocialLogo>
+          <WebsiteRights>eFIMa © 2020</WebsiteRights>
+          <SocialIcons>
+            <SocialIconLink
+              href={"https://www.facebook.com/search/top/?q=web%20development"}
+              rel="noopener noreferrer"
+              target="_blank"
+              aria-label="Facebook"
+            >
+              <FaFacebook />
+            </SocialIconLink>
+            <SocialIconLink
+              href={"https://www.instagram.com/javascriptmastery/"}
+              rel="noopener noreferrer"
+              target="_blank"
+              aria-label="Instagram"
+            >
+              <FaInstagram />
+            </SocialIconLink>
+            <SocialIconLink
+              href={
+                "https://www.youtube.com/results?search_query=web+development+"
+              }
+              rel="noopener noreferrer"
+              target="_blank"
+              aria-label="Youtube"
+            >
+              <FaYoutube />
+            </SocialIconLink>
+            <SocialIconLink
+              href={
+                "https://twitter.com/search?q=web%20development&src=typed_query"
+              }
+              rel="noopener noreferrer"
+              target="_blank"
+              aria-label="Twitter"
+            >
+              <FaTwitter />
+            </SocialIconLink>
+            <SocialIconLink
+              href={"https://www.linkedin.com/learning/topics/web-development"}
+              rel="noopener noreferrer"
+              target="_blank"
+              aria-label="Linkedin"
+            >
+              <FaLinkedin />
+            </SocialIconLink>
+          </SocialIcons>
+        </SocialMediaWrap>
+      </SocialMedia>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
